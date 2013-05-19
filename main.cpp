97bc55127918ef3fa510544dda252a13caf7a707
Serega <serega@Notebook.(none)>
2013-05-19 18:01:56 +0300

    Second commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: serega
+ *
+ * Created on 19 Май 2013 г., 13:08
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+cout << "Hello World! 19/05/13" << endl;
+    return 0;
+}
+
